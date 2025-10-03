e119c8d102d51e4e822e2bacc1c685c564a63b79
Vamekh Basharuli <vamekh@brandtotal.com>
2025-10-03 21:47:09 +0400

    [SOLID_OCP] Add Student, StudentsTest, and DistinctionDecider classes demonstrating OCP violation.

---
+++ src/test/java/ge/edu/ug/solid/ocp/students/StudentsTest.java
@@ -0,0 +1,20 @@
+package ge.edu.ug.solid.ocp.students;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class StudentsTest {
+    @Test
+    void testBadExample() {
+        Student tesla = new Student("Nikola", "Tesla", "Comp.Sc.", 80.0);
+        Student einstein = new Student("Albert", "Einstein", "Physics", 70.0);
+        Student darwin = new Student("Charles", "Darwin", "History", 60.0);
+        Student john = new Student("John", "Doe", "English", 50.0);
+        List<Student> students = List.of(tesla, einstein, darwin, john);
+
+        students.forEach(System.out::println);
+        DistinctionDecider decider = new DistinctionDecider();
+        students.forEach(decider::evaluateDistinction);
+    }
+}
